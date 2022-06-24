cacb84ad4661d966fc8b1e17f4b3e8e81ef2bb1b
mahdi-frms <mahdif1380@outlook.com>
2022-06-24 14:37:23 +0430

    fix formatting

--- notify.js
+++ notify.js
@@ -1,7 +1,6 @@
 import got from 'got'
 
-async function notify (pid)
-{
+async function notify(pid) {
     const server = process.env.SERVER;
     const URL = `${server}/api/project/${pid}/notify`;
     await got.post(URL);
