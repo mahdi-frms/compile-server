3b9eaf1708bafc47565c0fb553925d885eed68bb
mahdi-frms <mahdif1380@outlook.com>
2022-06-24 13:15:44 +0430

    POST /build API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import build from './build.js'
 import { } from 'dotenv/config'
 
 const app = express();
@@ -7,7 +8,12 @@ const { VERSION, PORT } = process.env
 
 app.get('/api/version', (req, res) => {
     res.json({ version: VERSION })
-})
+});
+
+app.post('/api/build/:pid', (req, res) => {
+    build(req.params.pid)
+    res.end()
+});
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
---
+++ build.js
@@ -0,0 +1,6 @@
+async function build(pid)
+{
+
+}
+
+export default build;
