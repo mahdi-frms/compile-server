8e39826ca8b0880ee0e9ab8cb413f2ab9f7a93b5
mahdi-frms <mahdif1380@outlook.com>
2022-06-26 10:51:37 +0430

    impl tasklist

---
+++ tasklist.js
@@ -0,0 +1,28 @@
+async function kmap(prm, key) {
+    const r = await prm;
+    return { key, result: r };
+}
+
+function never() {
+    return new Promise((res, rej) => { });
+}
+
+function Tasklist() {
+    this.tasks = []
+    this.map = {}
+
+    this.append = (key, task) => {
+        const idx = this.tasks.length;
+        this.map[key] = idx;
+        this.tasks[idx] = kmap(task, key);
+    }
+
+    this.wait = () => {
+        const { result, key } = await Promise.race(this.tasks);
+        const idx = this.map[key];
+        this.tasks[idx] = never();
+        return { result, key };
+    }
+}
+
+export default Tasklist;
